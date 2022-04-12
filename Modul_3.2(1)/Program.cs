ba9919d028f8becd84e50964f4eda3b1e0922f82
Макс Левченко <MaxiLeva@gmail.com>
2022-04-12 12:16:35 +0300

    Ура, я инициализировал переменную

--- Modul_3.2(1)/Program.cs
+++ Modul_3.2(1)/Program.cs
@@ -8,6 +8,12 @@ namespace Modul_3._2_1_
 {
     internal class Class1
     {
-
+        public static void Main(string[] args)
+        {
+            string MyNeme;
+            MyNeme = "Max";
+            Console.WriteLine(MyNeme);
+            Console.ReadKey();
+        }
     }
 }
