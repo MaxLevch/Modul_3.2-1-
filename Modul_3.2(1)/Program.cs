d1e1fd5acf2761293bdb465a552b0ec9755951a9
Макс Левченко <MaxiLeva@gmail.com>
2022-04-12 13:15:46 +0300

    типы переменных

--- Modul_3.2(1)/Program.cs
+++ Modul_3.2(1)/Program.cs
@@ -10,9 +10,17 @@ namespace Modul_3._2_1_
     {
         public static void Main(string[] args)
         {
-            string MyNeme;
-            MyNeme = "Max";
-            Console.WriteLine(MyNeme);
+            const string MyNeme = "Макс";
+            byte age = 28;
+            bool pet = false;
+            double size = 42.5;
+            Console.WriteLine("My neme is {0}", MyNeme);
+            Console.WriteLine("My age is {0}", age);
+            Console.WriteLine("Do I have pet? {0}", pet);
+            Console.WriteLine("My shoe size is {0}", size);
+
+            Console.WriteLine("\u0023 \u0001");
+
             Console.ReadKey();
         }
     }
