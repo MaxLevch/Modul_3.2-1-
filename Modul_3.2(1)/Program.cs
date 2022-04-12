2ccdf13b08c1e82efa109efc15609af849469b89
Макс Левченко <MaxiLeva@gmail.com>
2022-04-12 17:33:49 +0300

    3.7 Преобразование типов

--- Modul_3.2(1)/Program.cs
+++ Modul_3.2(1)/Program.cs
@@ -10,18 +10,46 @@ namespace Modul_3._2_1_
     {
         public static void Main(string[] args)
         {
-            const string MyNeme = "Макс";
+            /*const string MyNeme = "Макс";
             byte age = 28;
             bool pet = false;
             double size = 42.5;
             Console.WriteLine("My neme is {0}", MyNeme);
-            Console.WriteLine("My age is {0}", age);
+            Console.WriteLine("My age is " + age);
             Console.WriteLine("Do I have pet? {0}", pet);
-            Console.WriteLine("My shoe size is {0}", size);
+            Console.WriteLine($"My shoe size is {size}");
+            Console.WriteLine("Byte max = {0}", byte.MaxValue);*/
 
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked((byte) int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1}", name, age);
+            Console.Write("What is your favorite day of week ");
+            DayOfWeek day = (DayOfWeek) int.Parse(Console.ReadLine());
+            Console.WriteLine("Your faworite day is {0}", day);          
+            
+            
             Console.WriteLine("\u0023 \u0001");
 
             Console.ReadKey();
         }
     }
+}
+
+enum semafor
+{
+    Red = 100,
+    Green = 200,
+    Blue = 300
+}
+enum DayOfWeek
+{
+    Mondey = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturedey,
+    Sundey
 }
